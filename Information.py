0c4e3ec953ccd1cee63d0c7b7d7618f9aac68d0a
Iskander <128370358+kezuhiro-web@users.noreply.github.com>
2024-09-26 19:17:15 +0500

    Create Information.py

---
+++ Information.py
@@ -0,0 +1,51 @@
+# meta developer: @shiningwhore
+
+from .. import loader, utils
+from telethon import types
+
+class Information(loader.Module):
+    """Модуль для получения информации о пользователе или чате"""
+    strings = {"name": "Information"}
+
+    async def ainfo(self, message):
+        """Получить информацию о пользователе или чате"""
+        if message.is_reply:
+            user = await message.get_reply_message()
+            target = user.from_id
+        else:
+            args = utils.get_args_raw(message)
+            if not args:
+                await utils.answer(message, "Укажите username или ID пользователя.")
+                return
+            try:
+                target = await self.client.get_entity(args)
+            except Exception as e:
+                await utils.answer(message, f"Ошибка: {str(e)}")
+                return
+
+        if isinstance(target, types.User):
+            name = target.first_name or ""
+            last_name = target.last_name or ""
+            username = target.username or "None"
+            user_id = target.id
+            about = target.about or "None"
+            avatar = target.photo.large if target.photo else "Нет фото профиля"
+            common_groups = await self.get_common_groups(target)
+
+            result_message = f"{avatar}\n<b>Name:</b> {name} {last_name}\n<b>Username:</b> {username}\n<b>ID:</b> {user_id}\n<b>Bio:</b> {about}\n<b>Common groups:</b> {common_groups}"
+        
+        elif isinstance(target, types.Chat):
+            chat_name = target.title
+            chat_id = target.id
+            chat_link = f"<a href='tg://chat?id={chat_id}'>Link</a>"
+            avatar = target.photo.large if target.photo else "Нет аватарки"
+
+            result_message = f"{avatar}\n<b>Chat Name:</b> {chat_name}\n<b>ID:</b> {chat_id}\n<b>Link:</b> {chat_link}"
+        
+        await message.edit(result_message, parse_mode="html")
+
+    async def get_common_groups(self, user):
+        """Получить общее количество групп, в которых состоит пользователь"""
+        common_chats = await self.client.get_participants('user', limit=100)
+        common_groups = [chat.title for chat in common_chats if chat.is_group and chat.id in user.participation]
+        return ", ".join(common_groups) if common_groups else "Нет общих групп."
