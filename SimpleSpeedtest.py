c846536852e99260568cf56950e0143586b87b7e
Iskander <128370358+kezuhiro-web@users.noreply.github.com>
2024-09-30 08:22:39 +0500

    Update SimpleSpeedtest.py

--- SimpleSpeedtest.py
+++ SimpleSpeedtest.py
@@ -8,12 +8,12 @@ class SimpleSpeedtestMod(loader.Module):
 
     async def speedtestcmd(self, message):
         """Запускает тест скорости интернета"""
-        await message.edit("Замеряю скорость интернета...")
+        await message.edit("<b>Замеряю скорость интернета...</b>")
         try:
             result = subprocess.run(["speedtest", "--simple"], capture_output=True, text=True)
             if result.returncode != 0:
-                await message.edit(f"Произошла ошибка при выполнении speedtest: {result.stderr}")
+                await message.edit(f"<b>Произошла ошибка при выполнении speedtest:</b> {result.stderr}")
             else:
-                await message.edit(f"<b>Результат SpeedTest:</b>\n<pre>{result.stdout}</pre>")
+                await message.edit(f"<b>Результат Speedtest'a:</b>\n<pre>{result.stdout}</pre>")
         except Exception:
-            await message.edit(f"Произошла ошибка:\n<pre>{traceback.format_exc()}</pre>")
+            await message.edit(f"<b>Произошла ошибка:</b>\n<pre>{traceback.format_exc()}</pre>")
