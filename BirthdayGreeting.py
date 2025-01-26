f74994275e233e97857f2d241aae356b3f9c01cf
Kezuhiro <alexeimullvadov@gmail.com>
2025-01-26 21:00:20 +0500

    new module, changed banner in modules

---
+++ BirthdayGreeting.py
@@ -0,0 +1,52 @@
+#     ___    __ __ ____
+#    /   |  / //_//  _/
+#   / /| | / ,<   / /  
+#  / ___ |/ /| |_/ /   
+# /_/  |_/_/ |_/___/   
+#                      
+#     __  _______  ____  _____
+#    /  |/  / __ \/ __ \/ ___/
+#   / /|_/ / / / / / / /\__ \ 
+#  / /  / / /_/ / /_/ /___/ / 
+# /_/  /_/\____/_____//____/
+#
+# meta developer: @aki_user
+
+from .. import loader, utils
+import requests
+
+@loader.tds
+class BirthdayGreetingMod(loader.Module):
+
+    strings = {
+        "name": "BirthdayGreeting",
+        "error": "<b>An unexpected error occured...</b>",
+        "wait": "<b>Please wait, your greeting on generation...</b>",
+    }
+
+    strings_ru = {
+        "error": "<b>Произошла непредвиденная ошибка...</b>",
+        "wait": "<b>Пожалуйста подождите, ваше поздравление на генерации...</b>",
+    }
+
+    @loader.command(
+        ru_doc="[имя] - поздравить человека с Днём Рождения"
+    )
+    async def bdgreetcmd(self, message):
+        """[name] - congratulate a person on Birthday"""
+
+        name = utils.get_args_raw(message)
+        prompt = f"""Сгенерируй текст для поздравления с Днем рождения человека под именем {name}. Только текст для поздравления, один вариант."""
+        dictToSend = {"model": "gemini-flash", "request": {"messages": [{"role": "user", "content": prompt}]}}
+
+        try:
+            waiting_msg = await utils.answer(message, self.strings('wait'))
+
+            res = requests.post('http://api.onlysq.ru/ai/v2', json=dictToSend)
+            response = res.json()
+
+            waiting_msg.delete()
+            await utils.answer(message, response['answer'])
+
+        except Exception:
+            await utils.answer(message, self.strings('error'))
